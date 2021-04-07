dd80a5eb0a7297fa268e4bdcadcc4fd780be2305
simun.krmek <simun.krmek@fer.hr>
2021-04-07 17:37:26 +0200

    Dodan ostatak CF-a..treba jos urediti konstruktore

--- src/cuckoofilter.h
+++ src/cuckoofilter.h
@@ -1,7 +1,6 @@
 
 
 #include <string>
-
 #include "hash.h"
 #include "table.h"
 
@@ -20,6 +19,15 @@ class CuckooFilter {
   shared_ptr<table_type<bits_per_item>> table;
   size_t num_items;
 
+  class Victim{
+      public: 
+      size_t index;
+      size_t fingerprint;
+      bool used;
+  }
+    //U konstruktoru napravi victim.false;
+  Victim victim;
+
   hash_used hasher;
   static uint32_t mask = (1ULL << bits_per_item) - 1;
 
@@ -36,12 +44,15 @@ class CuckooFilter {
   }
 
   Status Add(const item_type& item) {
+      if (victim.used) return NotEnoughSpace;
+
     size_t index = GetIndex1(item);
     uint32_t fingerprint = GenerateFingerprint(item);
 
     return AddImpl(index, fingerprint);
   }
 
+    //Ako je usao u AddImpl, ubacit se item sigurno, ako nista na victimu ce netko biti
   Status AddImpl(const size_t index, const uint32_t fingerprint) {
     size_t current_index = index;
     uint32_t current_fingerprint = fingerprint;
@@ -63,9 +74,80 @@ class CuckooFilter {
 
       current_index = GetIndex2(current_index, current_fingerprint);
     }
-
-    return NotEnoughSpace;
+    //Koristim victima.. tablica je puna
+    victim.used=true;
+    victim.index=current_index
+    victim.fingerprint=current_fingerprint;
+    num_items++;
+    
+    return Ok;
   }
+
+  Status Contain(const item_type& item){
+      bool found=false;
+      size_t fingerprint=GenerateFingerprint(item);
+      size_t index1=GetIndex1(item);
+      size_t index2=GetIndex2(index1,fingerprint);
+
+    
+      found = (victim.used && victim.fingerprint==fingerprint && (index1==victim.index || index2=victim.index);
+      if (found || table-> FindFingerprintInBuckets(index1,index2,fingerprint)) return Ok;
+      else return NotFound;
+  }
+
+  Status Delete(const item_type& item){
+      size_t fingerprint=GenerateFingerprint(item);
+      size_t index1=GetIndex1(item);
+      size_t index2=GetIndex2(index1,fingerprint);
+    
+      if (table->deleteItemFromBucket(index1,fingerprint)){
+          num_items--;
+          if (victim.used){
+            victim.used=false;
+            AddImpl(victim.index,victim.fingerprint);
+            return Ok;
+          }
+      }
+      else if (table->deleteItemFromBucket(index2,fingerprint)){
+          num_items--;
+          if (victim.used){
+            victim.used=false;
+            AddImpl(victim.index,victim.fingerprint);
+            return Ok;
+          }
+      }
+      else if (victim.used && fingerprint == victim.fingerprint &&
+        (index1 == victim.index || index2 == victim.index)) {
+            num_items--;
+            victim.used = false;
+            return Ok;
+        }
+      return NotFound;
+    }
+
+  size_t Size() const { return num_items; }
+
+  size_t SizeInBytes() const { return table->SizeInBytes(); }
+
+  double LoadFactor() const { return 1.0 * Size() / table->SizeTable(); }
+
+  double BitsPerItem() const { return 8.0 * table->SizeInBytes() / Size(); }
+
+  string Info(){
+    std::stringstream ss;
+    ss << "CuckooFilter Status:\n"
+    << "\t\t" << table->Info() << "\n"
+    << "\t\tKeys stored: " << Size() << "\n"
+    << "\t\tLoad factor: " << LoadFactor() << "\n"
+    << "\t\tHashtable size: " << (table->SizeInBytes() >> 10) << " KB\n";
+    if (Size() > 0) {
+    ss << "\t\tbit/key:   " << BitsPerItem() << "\n";
+    } else {
+        ss << "\t\tbit/key:   N/A\n";
+    }
+    return ss.str();
+    }
+
 };
 
 }  // namespace cuckoofilter
--- src/table.h
+++ src/table.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 namespace cuckoofilter {
-  
+
 template <size_t bits_per_item>
 class Table {
   static const size_t k_items_per_bucket = 4;
@@ -29,16 +29,24 @@ class Table {
 
   size_t BucketCount() const { return bucket_count; }
 
-  uint32_t ReadItem(const size_t i, const size_t j) { return buckets[i][j]; }
-
-  uint32_t WriteItem(const size_t i, const size_t j, const uint32_t item) {
-    buckets[i][j] = item;
+  size_t SizeTable() const { 
+    return k_items_per_bucket * bucket_count; 
   }
 
-  bool DeleteItemFromBucket(const size_t i, const uint32_t item) {
+  size_t SizeInBytes() const { 
+    return k_bytes_per_bucket * bucket_count; 
+  }
+
+  uint32_t ReadItem(const size_t i, const size_t j) { return buckets[i][j]; }
+
+  uint32_t WriteItem(const size_t i, const size_t j, const uint32_t fingerprint) {
+    buckets[i][j] = fingerprint;
+  }
+
+  bool DeleteItemFromBucket(const size_t &index, const uint32_t &fingerprint) {
     for (size_t j = 0; j < k_items_per_bucket; j++) {
-      if (ReadItem(i, j) == item) {
-        WriteItem(i, j, 0);
+      if (ReadItem(index, j) == fingerprint) {
+        WriteItem(index, j, 0);
 
         return true;
       }
@@ -47,11 +55,19 @@ class Table {
     return false;
   }
 
-  bool InsertItemToBucket(const size_t i, const uint32_t item,
-                          const bool kickout, uint32_t &old_item) {
-    for (size_t j = 0; j < k_tags_per_bucket; j++) {
-      if (ReadTag(i, j) == 0) {
-        WriteTag(i, j, item);
+  bool FindFingerprintInBuckets(const size_t &index1,const size_t &index2,const size_t &fingerprint){
+    for (size_t j=0;j<k_items_per_bucket, j++){
+      if (ReadItem(index1,j)==fingerprint || ReadItem(index2,j)==fingerprint) return true;
+    }
+    return false;
+  }
+
+
+  bool InsertItemToBucket(const size_t &index, const uint32_t &fingerprint,
+                          const bool &kickout, uint32_t &old_fingerprint) {
+    for (size_t j = 0; j < k_items_per_bucket; j++) {
+      if (ReadItem(index, j) == 0) {
+        WriteItem(index, j, fingerprint);
 
         return true;
       }
@@ -59,12 +75,21 @@ class Table {
 
     if (kickout) {
       size_t r = rand() % k_items_per_bucket;
-      old_item = ReadTag(i, r);
-      WriteTag(i, r, item);
+      old_fingerprint = ReadItem(index, r);
+      WriteItem(index, r, fingerprint);
     }
 
     return false;
   }
-};
 
-};  // namespace cuckoofilter
+  std::string Info() const {
+    std::stringstream ss;
+    ss << "SingleHashtable with fingerprint size: " << bits_per_item << " bits \n";
+    ss << "\t\tAssociativity: " << k_items_per_bucket << "\n";
+    ss << "\t\tTotal # of rows: " << bucket_count<< "\n";
+    ss << "\t\tTotal # slots: " << SizeTable() << "\n";
+    return ss.str();
+  }
+
+};
+} // namespace cuckoofilter

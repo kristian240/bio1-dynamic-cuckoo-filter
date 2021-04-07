8112085b8c756dd166d8b4d49517758b857dc3b2
kristian240 <kristian.djakovic@infinum.hr>
2021-04-07 14:46:06 +0200

    Add add function

--- src/cuckoofilter.h
+++ src/cuckoofilter.h
@@ -1,40 +1,71 @@
 
 
-#include "table.h"
-#include "hash.h"
 #include <string>
 
-namespace cuckoofilter{
-const size_t max_num_kicks=500;
+#include "hash.h"
+#include "table.h"
 
-
-template<typename item_type=string, size_t bits_per_item=7,
-        template <size_t> class table_type= Table,
-        typename hash_used = Hash>
-class CuckooFilter{
-    shared_ptr<table_type<bits_per_item>> table;
-    size_t num_items;
-
-    hash_used hasher;
-    static uint32_t mask=(1ULL<<bits_per_item)-1;
-
-
-    //PAZI OVDJE RADI SAMO ZA STRING ZA SAD..
-    size_t generateFingerprint(item_type item){
-        return hasher(item) & mask;
-    }
-
-    size_t getindex1(item_type item){
-        //BUCKETCOUNT MI NE PREPOZNAJE
-        return hasher(item)%(table->BucketCount());
-    }
-    size_t getindex2(const size_t index1,cont uint32_t tag){
-         return ((index1 ^ (hasher(tag))%(table->BucketCount()));
-    }
-    
-
-
-
+namespace cuckoofilter {
+enum Status {
+  Ok = 0,
+  NotFound = 1,
+  NotEnoughSpace = 2,
 };
 
-}
+const size_t max_num_kicks = 500;
+
+template <typename item_type = string, size_t bits_per_item = 7,
+          template <size_t> class table_type = Table, typename hash_used = Hash>
+class CuckooFilter {
+  shared_ptr<table_type<bits_per_item>> table;
+  size_t num_items;
+
+  hash_used hasher;
+  static uint32_t mask = (1ULL << bits_per_item) - 1;
+
+  // PAZI OVDJE RADI SAMO ZA STRING ZA SAD..
+  size_t GenerateFingerprint(item_type& item) { return hasher(item) & mask; }
+
+  size_t GetIndex1(item_type& item) {
+    // BUCKETCOUNT MI NE PREPOZNAJE
+    return hasher(item) % (table->BucketCount());
+  }
+
+  size_t GetIndex2(const size_t& index1, const uint32_t& fingerprint) {
+    return (index1 ^ hasher(fingerprint)) % (table->BucketCount());
+  }
+
+  Status Add(const item_type& item) {
+    size_t index = GetIndex1(item);
+    uint32_t fingerprint = GenerateFingerprint(item);
+
+    return AddImpl(index, fingerprint);
+  }
+
+  Status AddImpl(const size_t index, const uint32_t fingerprint) {
+    size_t current_index = index;
+    uint32_t current_fingerprint = fingerprint;
+    uint32_t old_fingerprint;
+
+    for (uint32_t count = 0; count < max_num_kicks; count++) {
+      bool kickout = count > 0;
+
+      old_fingerprint = 0;
+
+      if (table->InsertItemToBucket(current_index, current_fingerprint, kickout,
+                                    old_fingerprint)) {
+        num_items++;
+
+        return Ok;
+      }
+
+      if (kickout) current_fingerprint = old_fingerprint;
+
+      current_index = GetIndex2(current_index, current_fingerprint);
+    }
+
+    return NotEnoughSpace;
+  }
+};
+
+}  // namespace cuckoofilter
--- src/hash.h
+++ src/hash.h
@@ -1,11 +1,12 @@
 #include <stdint.h>
+
 #include <string>
 
 #define get16bits(d) (*((const uint16_t *)(d)))
 
-namespace cuckoofilter{
-class Hash{
-    static uint32_t Hash::SuperFastHash(const void *buf, size_t len) {
+namespace cuckoofilter {
+class Hash {
+  static uint32_t Hash::SuperFastHash(const void *buf, size_t len) {
     const char *data = (const char *)buf;
     uint32_t hash = len, tmp;
     int rem;
@@ -17,27 +18,27 @@ class Hash{
 
     /* Main loop */
     for (; len > 0; len--) {
-        hash += get16bits(data);
-        tmp = (get16bits(data + 2) << 11) ^ hash;
-        hash = (hash << 16) ^ tmp;
-        data += 2 * sizeof(uint16_t);
-        hash += hash >> 11;
+      hash += get16bits(data);
+      tmp = (get16bits(data + 2) << 11) ^ hash;
+      hash = (hash << 16) ^ tmp;
+      data += 2 * sizeof(uint16_t);
+      hash += hash >> 11;
     }
 
     /* Handle end cases */
     switch (rem) {
-        case 3:
+      case 3:
         hash += get16bits(data);
         hash ^= hash << 16;
         hash ^= data[sizeof(uint16_t)] << 18;
         hash += hash >> 11;
         break;
-        case 2:
+      case 2:
         hash += get16bits(data);
         hash ^= hash << 11;
         hash += hash >> 17;
         break;
-        case 1:
+      case 1:
         hash += *data;
         hash ^= hash << 10;
         hash += hash >> 1;
@@ -52,11 +53,11 @@ class Hash{
     hash += hash >> 6;
 
     return hash;
-    }
+  }
 
-    public:
-    uint32_t operator()(const std::string &s) {
-        return SuperFastHash(s.data(), s.length());
-    }
+ public:
+  uint32_t operator()(const std::string &s) {
+    return SuperFastHash(s.data(), s.length());
+  }
 };
-}
+}  // namespace cuckoofilter

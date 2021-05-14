d0fae18f3632cd74370766cb34683dc580d53344
simun.krmek <simun.krmek@fer.hr>
2021-05-14 08:06:56 +0200

    Added delete, find, insert test table

--- test/table-test.cpp
+++ test/table-test.cpp
@@ -61,3 +61,57 @@ void test_get_bucket_table() {
   assert(bucket == v);
   std::cout << "PASS test_get_Bucket_table" << std::endl;
 }
+void test_delete_item_table() {
+  cuckoofilterbio1::Table<32> table(10);
+
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      table.WriteItem(i, j, i + j);
+    }
+  }
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      table.DeleteItemFromBucket(i, i + j);
+    }
+  }
+
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      assert(table.ReadItem(i, j) == 0);
+    }
+  }
+  std::cout << "PASS test_delete_item_table" << std::endl;
+}
+void test_find_fingerprints_in_buckets_table() {
+  cuckoofilterbio1::Table<32> table(10);
+
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      table.WriteItem(i, j, i + j);
+    }
+  }
+  assert(table.FindFingerprintInBuckets(0, 1, 4));
+  assert(!table.FindFingerprintInBuckets(0, 1, 6));
+  assert(table.FindFingerprintInBuckets(0, 7, 10));
+  assert(table.FindFingerprintInBuckets(2, 6, 5));
+  std::cout << "PASS test_find_fingerprints_in_buckets_table" << std::endl;
+}
+void test_insert_item_with_kickout_table() {
+  cuckoofilterbio1::Table<32> table(10);
+
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      table.WriteItem(i, j, i + j);
+    }
+  }
+  uint32_t old_fingerprint;
+  assert(table.InsertItemToBucket(0, 99, false, old_fingerprint));
+
+  assert(!table.InsertItemToBucket(7, 77, false, old_fingerprint));
+  assert(!table.FindFingerprintInBuckets(0, 7, 77));
+
+  assert(!table.InsertItemToBucket(5, 55, true, old_fingerprint));
+  assert(table.FindFingerprintInBuckets(0, 5, 55));
+
+  std::cout << "PASS test_insert_item_with_kickout_table" << std::endl;
+}

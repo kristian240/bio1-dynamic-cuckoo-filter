38d8de8db07169b79fce5bd69d10b8165d154dff
kristian240 <kristian.djakovic@infinum.hr>
2021-05-26 18:33:42 +0200

    Refactor kMer tests

--- test/kmer-test.cpp
+++ test/kmer-test.cpp
@@ -13,73 +13,82 @@ std::string str((std::istreambuf_iterator<char>(ecoli)),
     // std::cout << str << std::endl;
     ecoli1 << str; */
 
+std::string bases = "ACGT";
+int k_options[4] = {50, 100, 200, 500};
+
 void test1(int N) {
   std::cout << "TEST1" << std::endl;
+
   std::set<std::string> positive_set;
+
   std::ifstream ecoli1("ecoli1.txt");
+
   try {
     if (!ecoli1.is_open()) throw 1;
-    std::string genom;
+
     // Ucitaj genom
+    std::string genom;
     if (!getline(ecoli1, genom)) throw 2;
-    size_t len = genom.size();
-    int k_options[4] = {50, 100, 200, 500};
-    int random_variable;
-    int br_pos_set = 0;
-    int k, pos;
-    std::string kmer;
+    size_t genom_len = genom.size();
+
     while (positive_set.size() < N) {
-      random_variable = std::rand();
-      k = k_options[random_variable % 4];
-      pos = random_variable % len;
-      if (pos + k > len) continue;
-      kmer = genom.substr(pos, k);
+      int random_index = std::rand();
+      int k = k_options[random_index % 4];
+      int pos = random_index % genom_len;
+
+      if (pos + k > genom_len) continue;
+
+      std::string kmer = genom.substr(pos, k);
       positive_set.insert(kmer);
     }
   } catch (int i) {
     if (i == 1) std::cout << "Datoteka se nije otvorila " << std::endl;
     if (i == 2) std::cout << "Nisi u string ubacio genom" << std::endl;
   }
+
   ecoli1.close();
 }
+
 void test2(int N) {
   std::cout << "TEST2" << std::endl;
+
   std::set<std::string> positive_set;
   std::set<std::string> negative_set;
+
   std::ifstream ecoli1("ecoli1.txt");
+
   try {
     if (!ecoli1.is_open()) throw 1;
-    std::string genom;
+
     // Ucitaj genom
+    std::string genom;
     if (!getline(ecoli1, genom)) throw 2;
-    size_t len = genom.size();
-    int k_options[4] = {50, 100, 200, 500};
-    int random_variable;
-    int k, pos;
-    std::string bases = "ACGT";
-    std::string kmer;
-    std::string kmer_wrong;
+    size_t genom_len = genom.size();
+
     while (positive_set.size() < N || negative_set.size() < N) {
-      random_variable = std::rand();
-      k = k_options[random_variable % 4];
-      pos = random_variable % len;
-      if (pos + k > len) continue;
-      kmer = genom.substr(pos, k);
-      kmer_wrong = kmer;
-      char change_base;
+      int random_index = std::rand();
+      int k = k_options[random_index % 4];
+      int pos = random_index % genom_len;
+
+      if (pos + k > genom_len) continue;
+
+      std::string kmer = genom.substr(pos, k);
+      std::string kmer_wrong = kmer;
+
       for (int i = 0; i < 2; ++i) {
-        random_variable = std::rand() % k;
-        change_base = kmer_wrong.at(random_variable);
+        random_index = std::rand() % k;
+
+        char change_base = kmer_wrong.at(random_index);
         change_base = bases.at((bases.find(change_base) + rand() % 3 + 1) % 4);
-        kmer_wrong.at(random_variable) = change_base;
+        kmer_wrong.at(random_index) = change_base;
       }
-      if (positive_set.size() < N) {
-        positive_set.insert(kmer);
-      }
-      if (negative_set.size() < N &&
-          genom.find(kmer_wrong) == std::string::npos) {
+
+      if (positive_set.size() < N) positive_set.insert(kmer);
+
+      bool kmer_wrong_in_genom = genom.find(kmer_wrong) != std::string::npos;
+
+      if (negative_set.size() < N && !kmer_wrong_in_genom)
         negative_set.insert(kmer_wrong);
-      }
     }
 
   } catch (int i) {
@@ -89,40 +98,40 @@ void test2(int N) {
 
   ecoli1.close();
 }
+
 void test3(int N) {
   std::cout << "TEST3" << std::endl;
+
   std::set<std::string> positive_set;
   std::set<std::string> negative_set;
+
   std::ifstream ecoli1("ecoli1.txt");
+
   try {
     if (!ecoli1.is_open()) throw 1;
-    std::string genom;
+
     // Ucitaj genom
+    std::string genom;
     if (!getline(ecoli1, genom)) throw 2;
-    int k_options[4] = {50, 100, 200, 500};
-    int random_variable;
-    int k;
-    std::string bases = "ACGT";
+
     std::string kmer;
 
     while (positive_set.size() < N || negative_set.size() < N) {
-      random_variable = std::rand();
+      int random_variable = std::rand();
 
       // k = k_options[random_variable % 4];
-      k = 15;
+      int k = 15;
       kmer.clear();
       kmer.reserve(k);
+
       for (int i = 0; i < k; ++i) {
         kmer += bases.at(std::rand() % 4);
       }
+
       if (genom.find(kmer) != std::string::npos) {
-        if (positive_set.size() < N) {
-          positive_set.insert(kmer);
-        }
+        if (positive_set.size() < N) positive_set.insert(kmer);
       } else {
-        if (negative_set.size() < N) {
-          negative_set.insert(kmer);
-        }
+        if (negative_set.size() < N) negative_set.insert(kmer);
       }
     }
   } catch (int i) {
@@ -132,6 +141,7 @@ void test3(int N) {
 
   ecoli1.close();
 }
+
 int main(int argc, const char* argv[]) {
   std::srand(std::time(nullptr));
   test1(100);
